f6a3910414e8b11c7c24fa4c2073a65feac139f5
Alper Güçlü <alper@kuryaz.com>
2024-08-02 08:56:06 +0300

    markasread method added

--- src/imap/imap.service.ts
+++ src/imap/imap.service.ts
@@ -9,7 +9,7 @@ export class ImapService {
     this.imap = new Imap.default(this.config);
   }
 
-  openBox(boxName: string, readOnly: boolean): Promise<void> {
+  openBox(boxName: string, readOnly: boolean = true): Promise<void> {
     return new Promise((resolve, reject) => {
       this.imap.openBox(boxName, readOnly, (err) => {
         if (err) reject(err);
@@ -54,6 +54,18 @@ export class ImapService {
       return Promise.reject(new Error('UID must be a valid integer.'));
     }
     return this.getMessages([['UID', uid]], { bodies: ['HEADER', 'TEXT'], struct: true });
+  }
+
+  markAsRead(uid: number): Promise<void> {
+    if (!Number.isInteger(uid)) {
+      return Promise.reject(new Error('UID must be a valid integer.'));
+    }
+    return new Promise((resolve, reject) => {
+      this.imap.addFlags(uid, '\\Seen', (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
   }
 
   connect(): void {
